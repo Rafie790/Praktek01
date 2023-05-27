a0a16a21e9c49f7553c0dac87ac9d55b8d726d8f
Rafie790 <raficaesar3@gmail.com>
2023-05-27 22:49:21 +0800

    Membuat Class Aksi dan Method Main

---
+++ src/praktek01/MahasiswaAksi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktek01;
+
+/**
+ *
+ * @author Hanzo
+ */
+public class MahasiswaAksi {
+    public static void main(String[] args) {
+        
+    }
+    
+}
